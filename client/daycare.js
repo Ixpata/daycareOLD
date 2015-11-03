83ac6f075a5d1a5db7a53ef0e3294b303c86f787
Steve Mitchell <stevemitchell@Steves-MacBook-Air.local>
2015-11-02 20:24:02 -0500

    added input form

--- client/daycare.js
+++ client/daycare.js
@@ -1,7 +1,26 @@
+DaycareCenters = new Mongo.Collection("daycareCenters");
+
 Template.body.helpers({
-  daycareCenters: [
-    { text: "Bob's daycare" },
-    { text: "s"},
-    { text: "v" }
-  ]
+  daycareCenters: function () {
+    return DaycareCenters.find({}, {sort: {createdAt: -1}});
+  }
+});
+
+Template.body.events({
+  "submit .new-daycare-center": function (event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    var text = event.target.text.value;
+
+    // Insert a task into the collection
+    DaycareCenters.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });
+
+    // Clear form
+    event.target.text.value = "";
+  }
 });
---
+++ server/DaycareCenters.js
@@ -0,0 +1,1 @@
+DaycareCenters = new Mongo.Collection("daycareCenters");
