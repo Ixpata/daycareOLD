6ccd297f732f357250f5835b2ea0daf0c326b3cb
Steve Mitchell <stevemitchell@Steves-MacBook-Air.local>
2015-11-09 21:04:23 -0500

    http://meteorcapture.com/reactive-geolocation-with-google-maps/   Added google maps api and map image

--- client/daycare.js
+++ client/daycare.js
@@ -3,6 +3,19 @@ DaycareCenters = new Mongo.Collection("daycareCenters");
 Template.body.helpers({
   daycareCenters: function () {
     return DaycareCenters.find({}, {sort: {createdAt: -1}});
+  },
+
+  exampleMapOptions: function() {
+    // Make sure the maps API has loaded
+    var latLng = Geolocation.latLng();
+    if (GoogleMaps.loaded() && latLng) {
+      // Map initialization options
+      return {
+        //center: new google.maps.LatLng(-37.8136, 144.9631), // Geolocation.latLng();
+        center: new google.maps.LatLng(latLng.lat, latLng.lng),
+        zoom: 8
+      };
+    }
   }
 });
 
@@ -12,15 +25,18 @@ Template.body.events({
     event.preventDefault();
 
     // Get value from form element
-    var text = event.target.text.value;
+    var name = event.target.name.value;
+    var address = event.target.address.value;
 
     // Insert a task into the collection
     DaycareCenters.insert({
-      text: text,
+      name: name,
+      address: address,
       createdAt: new Date() // current time
     });
 
     // Clear form
-    event.target.text.value = "";
+    event.target.name.value = "";
+    event.target.address.value = "";
   }
 });
